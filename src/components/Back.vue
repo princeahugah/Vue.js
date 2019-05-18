2307cfa233a04c1ab7b7243341c74d76e65c29a1
Prince Ahugah <princenprinceg@gmail.com>
2019-05-18 00:04:00 +0000

    some vue components

---
+++ src/components/Back.vue
@@ -0,0 +1,36 @@
+<template>
+    <li class="tool-icon" ref="back" data-position="bottom" data-tooltip="Back" 
+        @click="navigateTo">
+        <i class="material-icons">chevron_left</i>
+    </li>
+</template>
+
+<script>
+export default {
+    name: 'Back',
+
+    props: {
+        options: {
+            type: Object,
+            default() {
+                return { location: '/' };
+            }
+        }
+    },
+    
+    mounted() {
+        this.setupTooltip();
+    },
+
+    methods: {
+        setupTooltip() {
+            const { Tooltip } = window.M;
+            Tooltip.init(this.$refs.back);
+        },
+
+        navigateTo() {
+            this.$router.push(this.options.location);
+        }
+    }
+};
+</script>
---
+++ src/components/FilterItem.vue
@@ -0,0 +1,31 @@
+<template>
+    <li @click="filterBy(filterItemName)">
+        <span>
+            <i v-show="activeItem === filterItemName" class="material-icons">check</i>
+            <slot></slot>
+        </span>
+    </li>
+</template>
+
+<script>
+export default {
+    name: 'FilterItem',
+
+    props: {
+        activeItem: {
+            type: String,
+            required: true
+        },
+
+        filterItemName: {
+            type: String,
+            required: true
+        },
+
+        filterBy: {
+            type: Function,
+            required: true 
+        }
+    }
+};
+</script>
